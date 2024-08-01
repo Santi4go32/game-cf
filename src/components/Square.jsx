a38a5187ef2c43fbfb2ca8519c8e17b8865d7dca
gcf-tech-santiago <strujillo@gcf.group>
2024-08-01 11:14:06 -0500

    add player image

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Square = ({ type, message, isPlayerHere, index }) => {
   return (
-    <div className={`square ${type} ${isPlayerHere ? 'player-here' : ''}`}>
+    <div className={`square ${type}`}>
       {message.image ? (
         <img src={`/${message.image}`} alt="Special" className="square-image" />
       ) : (
         <span>{message.text}</span>
       )}
+      {isPlayerHere && <img src="/player.png" alt="Player" className="player-image" />}
     </div>
   );
 };
